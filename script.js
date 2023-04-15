eca46a6e2189bd5eb777a125b66c7c71ab09502c
aditisinghal1210 <98580057+aditisinghal1210@users.noreply.github.com>
2023-04-15 21:38:59 +0530

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('visibilitychange',function(){
-    if(document.visibilityState=="visible"){
-        document.title="Active Tab";
-    }
-    else{
-        document.title="Inactive Tab"
-    }
-});
